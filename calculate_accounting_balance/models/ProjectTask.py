6231642add55e32c942362670d5219673dca1d0f
Son <sonthaibui@gmail.com>
2025-02-27 15:58:57 +0700

    Chỉnh kanban_state, default groupby my task, order kanban column (No), fitter category for app

--- calculate_accounting_balance/models/ProjectTask.py
+++ calculate_accounting_balance/models/ProjectTask.py
@@ -1,3 +1,10 @@
 from odoo import fields, models
 class ProjectTask(models.Model):
-    _inherit = 'project.task'        
+    _inherit = 'project.task'      
+    
+    project_stage_id = fields.Many2one(
+        'project.project.stage',  # Assuming the stage model is 'project.project.stage'
+        string='Project Stage',
+        related='project_id.stage_id',
+        store=True,  # Store the value in the database for grouping
+    )   
