4f53de2170ceb0190ab408a53c10b4d922a108fe
Son <sonthaibui@gmail.com>
2025-03-05 23:53:33 +0700

    xoa ham init

--- caosu/models/congthuckt.py
+++ caosu/models/congthuckt.py
@@ -8,7 +8,7 @@ class CTKT(models.Model):
     name = fields.Char(string='Name', required=True, copy=False)
     congthuc = fields.Html(string='Công thức')
 
-    def __init__(self, pool, cr):
+    '''def __init__(self, pool, cr):
         # Call the super method
         super(CTKT, self).__init__(pool, cr)
         # Check if the record with name 'Chưa bôi' exists
@@ -16,4 +16,4 @@ class CTKT(models.Model):
         exists = cr.fetchone()
         if not exists:
             # Create the record if it does not exist: lam khac kieu rubbertree
-            cr.execute("INSERT INTO ctkt (name, create_uid, create_date, write_uid, write_date) VALUES ('Chưa bôi', 1, NOW(), 1, NOW())")
+            cr.execute("INSERT INTO ctkt (name, create_uid, create_date, write_uid, write_date) VALUES ('Chưa bôi', 1, NOW(), 1, NOW())")'''
